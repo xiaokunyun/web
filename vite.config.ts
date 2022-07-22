8ef9b007a36eb1605a8afa111ecd6f4a43d9bd03
xuxiaokun <xuxiaokun@cnrmall.com>
2022-07-22 13:47:29 +0800

    2

--- vite.config.ts
+++ vite.config.ts
@@ -41,7 +41,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/',
+  base: '/web/',
   server: {
     host: '0.0.0.0',
     // open: true,
