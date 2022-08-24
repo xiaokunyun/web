f741af90cfdf8c41ca5b7c6435816161cd320303
xuxiaokun <xuxiaokun@cnrmall.com>
2022-08-24 15:41:35 +0800

    7

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
-import { createRouter, createWebHistory, RouteRecordRaw  } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import routes from '~pages'
+//RouteRecordRaw
 // RouteMeta
 // import { isMobileTerminal } from '@/util/flex'
 // import pc from './pc'//pc端路由表
@@ -35,7 +36,7 @@ import routes from '~pages'
 // ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: routes
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes
 })
 export default router
