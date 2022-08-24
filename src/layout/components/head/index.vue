d5eab0d5ea568343bf0bbba044b346a3325aad2c
xuxiaokun <xuxiaokun@cnrmall.com>
2022-08-24 15:44:08 +0800

    8

--- src/layout/components/head/index.vue
+++ src/layout/components/head/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-black h-5 w-screen overflow-hidden flex justify-center items-center">
-    <div class="text-white w-screen flex justify-center items-center">
+    <div class="bg-black h-5 w-screen overflow-hidden flex justify-center items-center">
+        <!-- <div class="text-white w-screen flex justify-center items-center">
       <div class="w-4/5 flex justify-center items-center">
         <div class="w-1/5" v-for="(item, index) in routes" :key="index">
           <span class="hover:text-dark-50">
@@ -8,11 +8,11 @@
           </span>
         </div>
       </div>
+    </div> -->
     </div>
-  </div>
 </template>
 <script setup lang="ts">
-import { routes } from "@/router";
+// import { routes } from "@/router";
 </script>
 
 
